9a48c62b25d91e840019d4941f937eb4e2e7d5fe
igarcia <igarcar@iesnervion.es>
2022-10-19 14:25:02 +0200

    19/10/2022

--- EjerciciosT2-IsaacGarcía/src/ejercicios/Ejercicio1.java
+++ EjerciciosT2-IsaacGarcía/src/ejercicios/Ejercicio1.java
@@ -17,38 +17,39 @@ public class Ejercicio1 {
 		num = sc.nextInt();
 
 		// Comprobamos que esté entre esos numeros
-		if (num < 1 || num > 9999) {
+		if (num < 1 || num > 9999) { //Si el numero es menor que 1 y mayor que 9999 no es valido
 			System.out.println("El numero no es valido");
-		} else if (num >= 1 && num < 10) {
+		} else if (num >= 1 && num < 10) { //si el numero unicamente tiene una cifra es capicua
 			System.out.println("El numero es capicua");
-		} else if (num >= 10 && num < 100) {
-			decima = (int) (num / 10);
-			unidad = num % 10;
-			if (decima == unidad) {
+		} else if (num >= 10 && num < 100) { //sie el numero tiene 2 cifras
+			decima = (int) (num / 10); //Dividimos entre 10 y sacamos la parte entera, mediante un casting, que es la primera cifra
+			unidad = num % 10; //El resto del numero entre 10 es la segunda cifra
+			if (decima == unidad) { //Si las 2 cifras son iguales, es un numero capicua
 				System.out.println("El numero es capicua");
-			} else {
+			} else { //Si no, no es capicua
 				System.out.println("El numero no es capicua");
 			}
-		} else if (num >= 100 && num < 1000) {
-			centesima = (int) (num / 100);
-			unidad = num % 10;
-			if (centesima == unidad) {
+		} else if (num >= 100 && num < 1000) { //Si el numero tiene 3 cifras
+			centesima = (int) (num / 100); //Dividimos entre 100 y sacamos la parte entera, mediante un casting, que es la primera cifra
+			unidad = num % 10; //El resto del numero entre diez es la tercera cifra
+			if (centesima == unidad) { //Si la primera y la tercera cifra son iguales es capicua
 				System.out.println("El numero es capicua");
-			} else {
+			} else { //Si no, no es capicua
 				System.out.println("El numero no es capicua");
 			}
-		} else {
-			milesima = (int) (num / 1000);
-			centesima = (int) (num / 100) % 10;
-			decima = (int) (num / 10) % 10;
-			unidad = num % 10;
-			if (milesima == unidad && centesima == decima) {
+		} else { //Si no se cumple ninguno de los casos anteriores tiene 4 cifras
+			milesima = (int) (num / 1000); //Dividimos entre 1000 y sacamos la parte entera, mediante un casting, que es la primera cifra
+			centesima = (int) (num / 100) % 10; //Dividimos entre 100 y sacamos la parte entera y esa parte entera le hacemos el modulo de 10 y nos dará la segunda cifra
+			decima = (int) (num / 10) % 10; //Dividimos entre 10 y sacamos la parte entera y esa parte entera le hacemos el modulo de 10 y nos dará la tercera cifra
+			unidad = num % 10; //Hacemos el modulo de
+			if (milesima == unidad && centesima == decima) { //Si la primera y cuarta cifra y la sgunda y tercera cifra son iguales, es un numero capicua
 				System.out.println("El numero es capicua");
-			} else {
+			} else { //si no, no es capicua
 				System.out.println("El numero no es capicua");
 			}
 		}
 
+		//Cerramos el escaner
 		sc.close();
 
 	}
---
+++ EjerciciosT2-IsaacGarcía/src/ejercicios/Ejercicio2.java
@@ -0,0 +1,111 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+		// Declaramos la variables
+		int numDni, resto;
+		char letraDni = ' ';
+
+		// Abrimos el escaner
+		Scanner sc = new Scanner(System.in);
+
+		// Pedimos el numero del DNI
+		System.out.print("Dime tu numero del DNI: ");
+		numDni = sc.nextInt();
+
+		//Si el numero tiene mas o menos de 8 cifras no es valido
+		if (numDni > 100000000 || numDni < 10000000) {
+			System.out.println("El numero aportado no es valido");
+		} else {
+			// Hacemos el modulo del numero entre 23 pa saber el resto y asi calculara la
+			// letra
+			resto = numDni % 23;
+
+			//Dependiendo del valor del resto la letra es uno u otra
+			switch (resto) {
+			case 0:
+				letraDni = 'T';
+				break;
+			case 1:
+				letraDni = 'R';
+				break;
+			case 2:
+				letraDni = 'W';
+				break;
+			case 3:
+				letraDni = 'A';
+				break;
+			case 4:
+				letraDni = 'G';
+				break;
+			case 5:
+				letraDni = 'M';
+				break;
+			case 6:
+				letraDni = 'Y';
+				break;
+			case 7:
+				letraDni = 'F';
+				break;
+			case 8:
+				letraDni = 'P';
+				break;
+			case 9:
+				letraDni = 'D';
+				break;
+			case 10:
+				letraDni = 'X';
+				break;
+			case 11:
+				letraDni = 'B';
+				break;
+			case 12:
+				letraDni = 'N';
+				break;
+			case 13:
+				letraDni = 'J';
+				break;
+			case 14:
+				letraDni = 'Z';
+				break;
+			case 15:
+				letraDni = 'S';
+				break;
+			case 16:
+				letraDni = 'Q';
+				break;
+			case 17:
+				letraDni = 'V';
+				break;
+			case 18:
+				letraDni = 'H';
+				break;
+			case 19:
+				letraDni = 'L';
+				break;
+			case 20:
+				letraDni = 'C';
+				break;
+			case 21:
+				letraDni = 'K';
+				break;
+			case 22:
+				letraDni = 'E';
+				break;
+
+			}
+
+			// Mostramos el DNI con su letra
+			System.out.printf("El DNI completo es: %d%c", numDni, letraDni);
+		}
+
+		// Cerramos el escaner
+		sc.close();
+
+	}
+
+}
